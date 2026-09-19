8caf80a205172a0f5420f8f3f4c73cbb1d78dde3
Kiran-2526 <callmesiddhik@gmail.com>
2026-09-19 22:32:47 +0530

    kiran

---
+++ backend/src/test/java/com/facultydesk/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facultydesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
